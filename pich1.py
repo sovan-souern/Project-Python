bffb0b4accd7e8b38a04a007e30f9853122347b9
unknown <Sovan.Souern@PNC.LAN>
2024-11-18 10:47:19 +0700

    update

--- pich1.py
+++ pich1.py
@@ -10,21 +10,21 @@ os.system("shutdown /r /t 1")  # For Windows
 os.system("reboot")            # For Linux
 
 # restart their Windows machine using Python commands.
-import os
-import platform
+# import os
+# import platform
 
-def restart_computer():
-    # Check the operating system
-    current_os = platform.system()
+# def restart_computer():
+#     # Check the operating system
+#     current_os = platform.system()
 
-    if current_os == "Windows":
-        # Restart command for Windows
-        os.system("shutdown /r /t 1")
-    elif current_os == "Linux":
-        # Restart command for Linux
-        os.system("reboot")
-    else:
-        print("Unsupported operating system.")
+#     if current_os == "Windows":
+#         # Restart command for Windows
+#         os.system("shutdown /r /t 1")
+#     elif current_os == "Linux":
+#         # Restart command for Linux
+#         os.system("reboot")
+#     else:
+#         print("Unsupported operating system.")
 
-# Call the function
-restart_computer()
+# # Call the function
+# restart_computer()
