5a680eb1ef825546cecf1f94ab722985bdaa15e4
phanhapich <panhapichrins@gmail.com>
2024-11-18 10:57:31 +0700

    change branch

--- pich1.py
+++ pich1.py
@@ -9,22 +9,22 @@ os.system("shutdown now")      # For Linux
 os.system("shutdown /r /t 1")  # For Windows
 os.system("reboot")            # For Linux
 
-# restart their Windows machine using Python commands.
-import os
-import platform
+# # restart their Windows machine using Python commands.
+# import os
+# import platform
 
-def restart_computer():
-    # Check the operating system
-    current_os = platform.system()
+# def restart_computer():
+#     # Check the operating system
+#     current_os = platform.system()
 
-    if current_os == "Windows":
-        # Restart command for Windows
-        os.system("shutdown /r /t 1")
-    elif current_os == "Linux":
-        # Restart command for Linux
-        os.system("reboot")
-    else:
-        print("Unsupported operating system.")
+#     if current_os == "Windows":
+#         # Restart command for Windows
+#         os.system("shutdown /r /t 1")
+#     elif current_os == "Linux":
+#         # Restart command for Linux
+#         os.system("reboot")
+#     else:
+#         print("Unsupported operating system.")
 
-# Call the function
-restart_computer()
+# # Call the function
+# restart_computer()
